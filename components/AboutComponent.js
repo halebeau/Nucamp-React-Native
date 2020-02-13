a6fea126943ec39de7b6f0293b4d0b75693af1af
Beau Hale <halebeau3@gmail.com>
2020-02-12 17:41:22 -0800

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import {connect} from 'react-redux';
 import {baseUrl} from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -25,19 +26,41 @@ class About extends Component {
         title: 'About Us'
     };
     
-    render() {
-        const renderPartner = ({item}) => {
+        render() {
+            const renderPartner = ({item}) => {
+                return (
+                    <ListItem
+                        title={item.name}
+                        subtitle={item.description}
+                        leftAvatar={{source: {uri: baseUrl + item.image}}}
+                    />
+                );
+            };
+    
+            if (this.props.partners.isLoading) {
+                return (
+                    <ScrollView>
+                        <Mission />
+                        <Card
+                            title='Community Partners'>
+                            <Loading />
+                        </Card>
+                    </ScrollView>
+                );
+            }
+            if (this.props.partners.errMess) {
+                return (
+                    <ScrollView>
+                        <Mission />
+                        <Card
+                            title='Community Partners'>
+                            <Text>{this.props.partners.errMess}</Text>
+                        </Card>
+                    </ScrollView>
+                );
+            }
             return (
-                <ListItem
-                    title={item.name}
-                    subtitle={item.description}
-                    leftAvatar={{source: {uri: baseUrl + item.image}}}
-                />
-            );
-        };
-        
-        return (
-            <ScrollView>
+                <ScrollView>
                 <Mission /> 
                 <Card
                 title="Community Partners"
@@ -52,4 +75,5 @@ class About extends Component {
         );
     }
 }
+
 export default connect(mapStateToProps)(About);
